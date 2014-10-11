f11f4d782085eeeff03f096775b22ff97488b893
Arnarka13 <arnarka13@ru.is>
2014-10-11 17:47:16 +0000

    Added unit test for more then twonumber where the user can choose to spilt the numbers by comma or newline

--- src/test/java/is/ru/StringCalculator/StringCalculatorTest.java
+++ src/test/java/is/ru/StringCalculator/StringCalculatorTest.java
@@ -21,5 +21,9 @@ public class StringCalculatorTest {
 	public void testTwoNumbers() {
 		assertEquals(3, StringCalculator.add("1,2"));
 	}
+	@Test
+	public void testMoreNumbersWithCommasAndNewLines() {
+		assertEquals(6, StringCalculator.add("1\n2,3"));
+	}
 
 }
